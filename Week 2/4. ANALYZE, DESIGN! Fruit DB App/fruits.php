82da08a7e2663931fd62f24be483c8f6e29dbdc4
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-10-03 18:05:08 -0400

    Complete 'Activity 1-5' (ANALYZE, DESIGN! Fruit DB App)

---
+++ Week 2/4. ANALYZE, DESIGN! Fruit DB App/fruits.php
@@ -0,0 +1,25 @@
+<?php
+  /*  fruits.php
+   *  ANALYZE, DESIGN! Fruit DB App
+   *  Server-Side Languages
+   *  Full Sail University
+   *  
+   *  Alexander Rhett Crammer
+   *  Saturday, 3 October, 2015  */
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ANALYZE, DESIGN! Fruit DB App</title>
+  </head>
+  <body>
+    <form method="post">
+      <label for="fruit_name">Name:</label><input name="fruit_name" id="fruit_name"><br />
+      <label for="fruit_colour">Colour:</label><input name="fruit_colour" id="fruit_colour"><br />
+      <input type="submit" value="Create">
+    </form>
+  </body>
+</html>
